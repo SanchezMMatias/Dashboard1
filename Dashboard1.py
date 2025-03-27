58c3a383a49c61d99f5eb378cd5bd519ff277aab
Matías Sánchez Muñoz <sanchezm.matias@gmail.com>
2025-03-27 11:08:50 -0300

    Update Dashboard1.py

--- Dashboard1.py
+++ Dashboard1.py
@@ -131,9 +131,9 @@ def load_and_prepare_data():
     """Carga y prepara los datos para el dashboard"""
     try:
         # Cargar los archivos Excel 
-        file_name_organizations = "detail-organizations-2025-03-25.xlsx"
-        file_name_subscriptions = "detail-subscription-2025-03-25.xlsx"
-        file_name_orders = "detail-order-2025-01-01-to-2025-03-24.xlsx"
+        file_name_organizations = "detail-organizations-2025-03-27.xlsx"
+        file_name_subscriptions = "detail-subscription-2025-03-27.xlsx"
+        file_name_orders = "detail-order-2025-01-01-to-2025-03-27.xlsx"
         
         df_organizations = pd.read_excel(file_name_organizations, sheet_name="Organizations")
         df_subscriptions = pd.read_excel(file_name_subscriptions)
